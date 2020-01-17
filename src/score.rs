5604ddf35c8312fdd9bd5d336ee79389f12f6d13
Justin Barclay <justincbarclay@gmail.com>
2020-01-16 21:31:05 -0700

    Calculate and display total effort score and estimated effort score

--- src/score.rs
+++ src/score.rs
@@ -4,7 +4,7 @@ use std::collections::HashMap;
 use dialoguer::Select;
 use prettytable::Table;
 use regex::Regex;
-
+use regex::Captures;
 use crate::trello::{Board, Card, Auth, List};
 
 /// A deck represents some summary data about a list of Trello cards
@@ -13,11 +13,21 @@ pub struct Deck{
   pub name: String,
   // Represents total numbers of cards in the list
   pub size: usize,
-  // Represents the cumulative total score of all the cards in the lists
+  // Represents the cumulative total effort of all the cards in the list
   pub score: i32,
   // Represents the total amount of unscored cards in the list
-  pub unscored: i32
+  pub unscored: i32,
+  // Represents the estimated effort for all cards in the list during the sprint
+  pub estimated: i32
 
+}
+
+/// A score is a result of a user estimating the effort required for a card `()` and then optionally
+/// a correction `[]` after they've completed the card and found out it was worth more or less effort.
+#[derive(PartialEq, Debug)]
+pub struct Score{
+  estimated: Option<i32>,
+  correction: Option<i32>
 }
 
 /// Allows the user to select a board from a list
@@ -85,80 +95,91 @@ pub async fn build_decks(auth: Auth, lists: Vec<List>) ->  Result< Vec<Deck>, Bo
       .get(&format!("https://api.trello.com/1/lists/{}/cards?card_fields=name&key={}&token={}", list.id, auth.key, auth.token))
       .send()
       .await?
-    .json()
+      .json()
       .await?;
+
+    let (score, unscored, estimated) = cards.iter().fold((0,0,0), |(total, unscored, estimate), card|{
+      match get_score(&card.name){
+        Some(score) => {
+          if let Some(correction) = score.correction {
+            (total + correction, unscored, estimate)
+          } else {
+            (total + score.estimated.unwrap(), unscored, estimate + score.estimated.unwrap())
+          }
+        },
+        None => (total, unscored + 1, estimate)
+      }
+    });
 
     decks.push(
       Deck{
         name: list.name,
         size: cards.len(),
-        score: cards.iter().fold(0, |total, card|{
-          match get_score(&card.name){
-            Some(score) => total + score,
-            None => total
-          }
-
-        }),
-        unscored: cards.iter().fold(0, |total, card| {
-          match get_score(&card.name){
-            Some(_) => total,
-            None => total + 1
-          }
-        })
+        score,
+        unscored,
+        estimated,
       });
   }
 
   Ok(decks)
 }
 
-/// Extracts a score from a trello card, based on using [] or (). If no score is found a 0 is returned
-fn get_score(maybe_points: &str) -> Option<i32>{
-  // this will capture on "(0)" or "[0]" where 0 is an arbitrary sized digit
-  let square_brackets = Regex::new(r"\[(\d+)\]").unwrap();
-  let round_brackets = Regex::new(r"\((\d+)\)").unwrap();
-  let cap = match square_brackets.captures(&maybe_points) {
-    Some(cap) => cap,
-    // Early exit
-    None => {
-      match round_brackets.captures(&maybe_points){
-        Some(other_cap) => other_cap,
-        None => return None
-      }
-    }
-  };
+/// Converts a trello effort score either [\d] or (\d) into a number.
+/// If the item inside the brackets can not be converted into a number,
+/// return None instead.
+fn score_to_num(capture: Option<Captures>) -> Option<i32>{
+  // If at any point this fails we should return None
+  capture
+    .map(|cap| cap.get(0).unwrap())
+    .map(|parsed_string| {
+      let maybe_score = String::from(parsed_string.as_str());
+      let maybe_number = &maybe_score[1..maybe_score.len()-1];
+      maybe_number.parse::<i32>().unwrap()
+    })
+    .map(|number| number)
+}
 
-  match cap.get(0) {
-    Some(capture) =>{
-      let a_match = String::from(capture.as_str());
-      // We need to strip the brackets
-      let maybe_number = &a_match[1..a_match.len()-1];
-      match maybe_number.parse::<i32>() {
-        Ok(number) => Some(number),
-        Err(_) => None
-      }
-    },
-    None => None
+/// Extracts a score from a trello card, based on using [] or (). If no score is found a 0 is returned
+fn get_score(maybe_points: &str) -> Option<Score>{
+  // this will capture on "(0)" or "[0]" where 0 is an arbitrary sized digit
+  let correction = score_to_num(Regex::new(r"\[(\d+)\]")
+                                .unwrap()
+                                .captures(&maybe_points));
+
+  let estimated = score_to_num(Regex::new(r"\((\d+)\)")
+                               .unwrap()
+                               .captures(&maybe_points));
+
+  if let (None, None) = (estimated, correction){
+    return None;
   }
+
+  Some(
+    Score{
+      estimated,
+      correction
+    })
 }
 
 pub fn print_decks(decks: Vec<Deck>) -> (){
   let mut table = Table::new();
 
-  table.add_row(row!["List", "cards", "score", "unscored"]);
+  table.add_row(row!["List", "cards", "score","estimatedx", "unscored"]);
 
   for deck in decks {
-    table.add_row(row![deck.name, deck.size, deck.score, deck.unscored]);
+    table.add_row(row![deck.name, deck.size, deck.score, deck.estimated, deck.unscored]);
   }
 
   table.printstd();
 }
 
 pub mod test{
-  use super::get_score;
+  #[allow(unused_imports)]
+  use super::{get_score, Score};
 
   #[test]
   fn get_score_handles_curlies(){
-    assert_eq!(get_score("(10)"), Some(10));
+    assert_eq!(get_score("(10)").unwrap().estimated, Some(10));
 
     assert_eq!(get_score("()"), None);
 
@@ -168,7 +189,7 @@ pub mod test{
 
   #[test]
   fn get_score_handles_angles(){
-    assert_eq!(get_score("[10]"), Some(10));
+    assert_eq!(get_score("[10]").unwrap().correction, Some(10));
 
     assert_eq!(get_score("[]"), None);
 
@@ -178,29 +199,20 @@ pub mod test{
 
   #[test]
   fn get_score_handles_curlies_and_angles(){
-    assert_eq!(get_score("[10](9)"), Some(10));
-
+    assert_eq!(get_score("[10](9)").unwrap().correction, Some(10));
+    assert_eq!(get_score("[10](9)").unwrap().estimated, Some(9));
     assert_eq!(get_score("[]()"), None);
 
-    assert_eq!(get_score("[z](9)"), Some(9));
-    assert_eq!(get_score("[9](z)"), Some(9));
-    assert_eq!(get_score("[](9)"), Some(9));
-    assert_eq!(get_score("[9]()"), Some(9));
+    assert_eq!(get_score("[z](9)").unwrap().estimated, Some(9));
+    assert_eq!(get_score("[9](z)").unwrap().correction, Some(9) );
+    assert_eq!(get_score("[](9)").unwrap().estimated, Some(9));
+    assert_eq!(get_score("[9]()").unwrap().correction, Some(9));
     assert_eq!(get_score("[9z]()"), None);
   }
 
   #[test]
   fn get_score_handles_arbitrarily_sized_digits(){
-    assert_eq!(get_score("[100000000](9)"), Some(100000000));
-  }
-
-  #[test]
-  fn get_score_prioritizes_square_brackets(){
-    // Square brackets should be prioritized over round brackets
-    assert_eq!(get_score("(9)[10]"), Some(10));
-    assert_eq!(get_score("[10](9)"), Some(10));
-
-    assert_eq!(get_score("(10)[9]"), Some(9));
-    assert_eq!(get_score("[9](10)"), Some(9));
+    assert_eq!(get_score("[100000000](9)").unwrap().correction, Some(100000000));
+    assert_eq!(get_score("[100000000](9)").unwrap().estimated, Some(9));
   }
 }
