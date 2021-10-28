d55aa7ebb8ea5d93dc0e94ee590cb1fd3bce5c86
Justin Barclay <justincbarclay@gmail.com>
2021-10-28 16:46:44 -0700

    Update deploy lambda script

--- card-counter/lambda/deploy-lambda.sh
+++ card-counter/lambda/deploy-lambda.sh
@@ -4,9 +4,9 @@ cur_dir=$PWD
 cd ../../
 cross build --release -p burndown-lambda --target x86_64-unknown-linux-musl
 
-cp ./lambda/target/x86_64-unknown-linux-musl/release/burndown-lambda ../../lambda/target/x86_64-unknown-linux-musl/release/bootstrap
+cp ./target/x86_64-unknown-linux-musl/release/burndown-lambda ./target/x86_64-unknown-linux-musl/release/bootstrap
 
-zip -j card-counter/lambda/rust.zip ./target/x86_64-unknown-linux-musl/release/bootstrap
+zip -j card-counter/lambda/rust.zip . /target/x86_64-unknown-linux-musl/release/bootstrap
 
 cd $cur_dir
 sam build
